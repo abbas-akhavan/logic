6274764208c7a7b323c877e1fb9f2a91ec28e3ac
pooyan <thepooyan@gmail.com>
2023-08-27 16:44:48 +0330

    course video function done

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,21 @@
 
+$(function() {
+    //event of videos inside course content (journal)
+    const courseVideos = document.querySelectorAll('.courseVideo');
+    if (courseVideos.length > 0)
+    courseVideos.forEach(videoWrap => {
+        const video = videoWrap.querySelector('video');
+
+        videoWrap.onclick = () => {
+            videoWrap.classList.add('active');
+            video.controls = true;
+        }
+
+        video.onended = () => {
+            videoWrap.classList.remove('active');
+            video.controls = false;
+        }
+    })
+
+    
+})
