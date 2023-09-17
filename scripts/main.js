3a276ebdb89017b870f2ebbf311c4f82fe2e8af6
pooyan <thepooyan@gmail.com>
2023-09-17 11:36:18 +0330

    getup created

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,33 @@
+$(function () {
+  //event of videos inside course content (journal)
+  const courseVideos = document.querySelectorAll(".courseVideo");
+  if (courseVideos.length > 0)
+    courseVideos.forEach((videoWrap) => {
+      const video = videoWrap.querySelector("video");
 
-$(function() {
-    //event of videos inside course content (journal)
-    const courseVideos = document.querySelectorAll('.courseVideo');
-    if (courseVideos.length > 0)
-    courseVideos.forEach(videoWrap => {
-        const video = videoWrap.querySelector('video');
+      videoWrap.onclick = () => {
+        videoWrap.classList.add("active");
+        video.controls = true;
+        video.play();
+      };
 
-        videoWrap.onclick = () => {
-            videoWrap.classList.add('active');
-            video.controls = true;
-            video.play();
-        }
+      video.onended = () => {
+        videoWrap.classList.remove("active");
+        video.controls = false;
+      };
+    });
 
-        video.onended = () => {
-            videoWrap.classList.remove('active');
-            video.controls = false;
-        }
-    })
+  //getUp
+  const getUp = dc.id("getUp");
+  if (getUp) {
+    const checkScroll = () => {
+      if (window.scrollY > window.innerHeight) getUp.classList.add("active");
+      else getUp.classList.remove("active");
+    };
+    getUp.onclick = () => {
+      window.scrollTo(0, 0);
+    };
 
-    
-})
+    window.addEventListener("scroll", checkScroll);
+  }
+});
